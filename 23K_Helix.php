50cd9da28aac38ba6a59386360e9b6b534acc8ce
Julie Mitchell <julie@23k.com>
2014-03-27 11:50:50 -0400

    Updated to 1.3.033

--- 23K_Helix.php
+++ 23K_Helix.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: 23K Helix
-Version: 1.3.032
+Version: 1.3.033
 Description: A robust website platform for use with the 23K Darwin adaptive framework.
 Author: 23K Studios
 Author URI: http://23K.com
